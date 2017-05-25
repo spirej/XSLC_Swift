0536411c04915d0af7759fd397d94e16d179b846
fushengjun <fushengjun@xiangshang360.com>
2017-05-25 11:25:19 +0800

    从xib加载自定义View

---
+++ XSLC_Swift/Mine/View/XSCustomHeader.swift
@@ -0,0 +1,21 @@
+//
+//  XSCustomHeader.swift
+//  XSLC_Swift
+//
+//  Created by SPIREJ on 2017/5/25.
+//  Copyright © 2017年 SPIREJ. All rights reserved.
+//
+
+import UIKit
+
+class XSCustomHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
